9d90dd57b493a7b8327ddc8c1c1cb150d59c1d2c
Dovydas J <dovydas.jaseliunas@gmail.com>
2022-08-09 15:23:35 +0300

    Validate appointment data

--- tests/booking.js
+++ tests/booking.js
@@ -5,29 +5,43 @@ module.exports = {
             .url('https://katalon-demo-cura.herokuapp.com/')
             //Click on toggle menu
             .click('#menu-toggle')
-            //Click login
+           
+            //Login
             .click("a[href*='login']")
-            //Input username
             .setValue('#txt-username','John Doe')
-            //Input password
             .setValue('#txt-password','ThisIsNotAPassword')
-            //Click login button
             .click('#btn-login')
-            //Click facility dropdown
-            .click('#combo_facility')
-            //Select facility
-            .click("option[value='Seoul CURA Healthcare Center']")
-            //Click checkbox
-            .click('#chk_hospotal_readmission')
-            //Click radio select
-            .click('#radio_program_medicaid')
-            //Set visit date
-            .setValue('#txt_visit_date','02/11/2021')
-            //Set comment 
-            .setValue('#txt_comment','This is a test appointment')
 
-            //Validate correct error message
-            .assert.visible('#facility','Seoul CURA Healthcare Center')
+            //Book an appoitment
+            .click('#combo_facility')
+            .click("option[value='Seoul CURA Healthcare Center']")
+            .click('#chk_hospotal_readmission')
+            .click('#radio_program_medicaid')
+            .setValue('#txt_visit_date','02/11/2021')
+            .setValue('#txt_comment','This is a test appointment')
+            .click('#btn-book-appointment')
+            
+            //Validate appointment data
+            .assert.containsText('#facility','Seoul CURA Healthcare Center')
+            .assert.containsText('#hospital_readmission','Yes')
+            .assert.containsText('#facility','Seoul CURA Healthcare Center')
+            .assert.containsText('#program','Medicaid')
+            .assert.containsText('#visit_date','02/11/2021')
+            .assert.containsText('#comment','This is a test appointment')
+  
+            //Logout
+            .click('#menu-toggle')
+            .click("a[href*='logout']")
+
+            //validate that app info is no longer displayed
+            .assert.not.elementPresent('#facility','Seoul CURA Healthcare Center')
+            .assert.not.elementPresent('#hospital_readmission','Yes')
+            .assert.not.elementPresent('#facility','Seoul CURA Healthcare Center')
+            .assert.not.elementPresent('#program','Medicaid')
+            .assert.not.elementPresent('#visit_date','02/09/2021')
+            .assert.not.elementPresent('#comment','This is a test appointment')
+
+
 
         
 
