1c68e7e9c36544d82e6745db71e7d85f04dffd9c
Dovydas J <dovydas.jaseliunas@gmail.com>
2022-08-10 10:50:41 +0300

    Seperate registration data from the object

--- pageObjects/yahooPage.js
+++ pageObjects/yahooPage.js
@@ -13,12 +13,20 @@ const yahooCommands = {
             this.setValue('@emailField',email)
             this.click('@submitSignIn')
     },
-    validateErrorMesage: function(message){
+    validateErrorMessage: function(element,message){
         //Validate error message
-        this.assert.visible('@emailErrorMessage')
-        this.assert.containsText('@emailErrorMessage',message)
-
-    }
+        this.assert.visible(element)
+        this.assert.containsText(element,message)
+    },
+    attemptRegistration: function () {
+        this.click('@signInButton')
+        this.click('@createAccButton')
+        this.setValue('@firstName', 'John')
+        this.setValue('@email', 'test758492')
+        this.setValue('@password', '4673857Ajsdhdbf')
+        this.setValue('@birthYear', '1990')
+        this.click('@submitRegistration')
+      }
 
 }
 
@@ -31,6 +39,13 @@ module.exports = {
         signInButton:"div[role='toolbar'] a[href*='login']",
         emailField: '#login-username',
         submitSignIn:'#login-signin',
-        emailErrorMessage:'#username-error'
+        emailErrorMessage:'#username-error',
+        createAccButton: '#createacc',
+        firstName: '#usernamereg-firstName',
+        email: '#usernamereg-userId',
+        password: '#usernamereg-password',
+        birthYear: '#usernamereg-birthYear',
+        submitRegistration: '#reg-submit-button',
+        lastNameErrorMessage: '#reg-error-lastName'
     }
 }
--- tests/yahoo.js
+++ tests/yahoo.js
@@ -1,35 +1,16 @@
 module.exports = {
-    'Sign in - Enter invalid email': client => {
-        let yahooPage = client.page.yahooPage();
+    // 'Sign in - Enter invalid email': client => {
+    //     let yahooPage = client.page.yahooPage();
        
+    //     yahooPage.prepareWebsite();
+    //     yahooPage.attemptSignIn('nodemoemail.com');
+    //     yahooPage.validateErrorMesage("Sorry, we don't recognize this email.");
+    // },
+    'Sign up - Leave Last name empty': client => {
+        let yahooPage = client.page.yahooPage();
+    
         yahooPage.prepareWebsite();
-        yahooPage.attemptSignIn('nodemoemail.com');
-        yahooPage.validateErrorMesage("Sorry, we don't recognize this email.");
-    },
-
-
-    // 'Sign Up - Leave Last name empty': client =>{
-    //     client
-    //     //Open the page
-    //       .url('https://www.yahoo.com/')
-    //     //Accept cookies
-    //         .click("button[name='agree']")
-    //         .waitForElementVisible('#Header',4000)
-    //     //Navigate to Sign In
-    //         .click("div[role='toolbar'] a[href*='login']")  
-    //     //Navigate to account creation
-    //         .click('#createacc')
-    //     //Fill in the fields
-    //         .setValue('#usernamereg-firstName','Jonas')
-    //         .setValue('#usernamereg-userId','test')
-    //         .setValue('#usernamereg-password','Jonas10203040')
-    //         .setValue('#usernamereg-birthYear','1999')
-    //         .click('#reg-submit-button')
-    //     //Validate correct error message
-    //     .assert.visible('reg-error-lastName')
-    //     .assert.containsText('#reg-error-lastName','This is required')
-
-
-
-    // }
+        yahooPage.attemptRegistration();
+        yahooPage.validateErrorMessage('@lastNameErrorMessage', 'This is required');
+    }
 }
